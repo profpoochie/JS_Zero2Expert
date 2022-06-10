1651f29957af0b539f30c626c5dc4bb0ac9680e0
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2022-06-11 05:17:11 +0800

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,11 @@
 let js = "amazing";
-if (js === "amazing") {
-  alert("JavaScript is FUN!");
-}
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let PI = 3.1415;
