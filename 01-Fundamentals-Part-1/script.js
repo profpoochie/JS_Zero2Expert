0034f6abac18fdd4259e4ce34e2a88db94105c57
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2022-06-11 22:17:22 +0800

    Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,11 +1,33 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
 
-let jonas_matilda = "JM";
-let PI = 3.1415;
+// let jonas_matilda = "JM";
+// let PI = 3.1415;
+
+// true;
+// console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
