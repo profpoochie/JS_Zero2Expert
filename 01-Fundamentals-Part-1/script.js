859e1ce3ffb17d296c2d44a0823e8f51f859dfb3
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2022-06-11 22:53:43 +0800

    Basic Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -30,7 +30,7 @@ year = 1991;
 console.log(year);
 console.log(typeof year);
 
-console.log(typeof null); */
+console.log(typeof null); 
 
 let age = 30;
 age = 31;
@@ -44,3 +44,32 @@ job = "teacher";
 
 lastName = "Borhillo";
 console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
