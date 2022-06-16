44e7b046b7b549122488de83db3d1adb2972ff38
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2022-06-16 09:51:42 +0800

    Coding Challenge 3

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -217,7 +217,7 @@ const age = "18";
 if (age === 18) console.log("You just became and adult. strict");
 
 if (age == 18) console.log("You just became and adult. loose");
-*/
+
 
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
@@ -238,4 +238,25 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log("Sarah is able to drive!");
 } else {
   console.log("Someone else should drive...");
+}
+*/
+
+// Coding Challenge #3
+const scoreDolphins1 = 96;
+const scoreDolphins2 = 108;
+const scoreDolphins3 = 89;
+
+const scoreKoalas1 = 88;
+const scoreKoalas2 = 91;
+const scoreKoalas3 = 110;
+
+const averageDolphins = (scoreDolphins1 + scoreDolphins2 + scoreDolphins3) / 3;
+const averaKoalas = (scoreKoalas1 + scoreKoalas2 + scoreKoalas3) / 3;
+
+console.log(`Dolphine average = ${averageDolphins}
+Koalas average = ${averaKoalas}`);
+if (averageDolphins > averaKoalas) {
+  console.log("Dolphine wins.");
+} else {
+  console.log("Koalas wins.");
 }
