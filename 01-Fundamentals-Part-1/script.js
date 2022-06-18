47cc61debd800773f31416f7c050c1f18a730561
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2022-06-18 12:34:41 +0800

    switch statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -239,7 +239,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive...");
 }
-*/
+
 
 // Coding Challenge #3
 const scoreDolphins1 = 96;
@@ -262,3 +262,49 @@ if (averageDolphins > averageKoalas) {
 } else {
   console.log("It's a tie.");
 }
+
+
+
+//const day = "monday";
+
+// switch (day) {
+//   case "monday": // day === 'monday'
+//     console.log("Plan couse structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+const day = "saturday";
+
+if (day === "monday") {
+  console.log("Plan couse structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+
+*/
